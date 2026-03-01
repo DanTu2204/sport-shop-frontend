ef1486eff3d281d91cc11b9b6614cc58891608e3
Tú <dantutp@gmail.com>
2026-03-01 14:35:35 +0700

    deploy1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://sport-shop-backend.onrender.com',
     withCredentials: true, // important for session cookies
     headers: {
         'Content-Type': 'application/json',
