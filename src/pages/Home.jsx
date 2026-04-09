6910ee596235581565e31d03a6453a19bf035ea3
Tú <dantutp@gmail.com>
2026-04-10 02:29:51 +0700

    deploy19

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,18 +73,7 @@ function Home() {
                       </div>
                     </div>
                   </div>
-                )) : (
-                  <div className="carousel-item position-relative active" style={{ height: '430px' }}>
-                    <img className="position-absolute w-100 h-100" src="/img/carousel-1.jpg" style={{ objectFit: 'cover' }} alt="Banner" />
-                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                      <div className="p-3" style={{ maxWidth: '700px' }}>
-                        <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men Fashion</h1>
-                        <p className="mx-md-5 px-5 animate__animated animate__bounceIn">SportShop Collection</p>
-                        <Link className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" to="/shop">Shop Now</Link>
-                      </div>
-                    </div>
-                  </div>
-                )}
+                )) : null}
               </div>
             </div>
           </div>
